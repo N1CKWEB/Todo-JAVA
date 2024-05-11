881d4021e79b49eb596e02c1bb0fc60f87a4d46d
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2024-05-11 18:45:57 -0300

    Sistema de Agenda Electronica con BD, realizando un CRUD

---
+++ Conexion/src/SistemaDeAgendaElectronica/Conexion.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package SistemaDeAgendaElectronica;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Conexion {
+
+    String url = "jdbc:mysql://localhost:3306/sistema_agenda_electronica";
+    String user = "BDNico";
+    String password = "";
+    String driver = "com.mysql.cj.jdbc.Driver";
+    Connection cx;
+
+    public Conexion(Connection cx) {
+        this.cx = cx;
+    }
+
+    public Conexion() {
+
+    }
+
+    public Connection conectar() {
+
+        try {
+            Class.forName(driver);
+            cx = DriverManager.getConnection(url, user, password);
+
+            JOptionPane.showMessageDialog(null, "SE CONECTO A BD");
+          
+        } catch (ClassNotFoundException | SQLException e) {
+
+            JOptionPane.showMessageDialog(null, "NO CONECTO A BD");
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, e);
+
+        }
+        return cx;
+    }
+
+    public void desconectar() {
+        try {
+            cx.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void agregarUsuarioABD() {
+       
+    }
+
+    public static void main(String[] args) {
+        
+  
+        Conexion conexion = new Conexion();
+        conexion.conectar();
+        
+            
+   
+
+   
+    }
+}
