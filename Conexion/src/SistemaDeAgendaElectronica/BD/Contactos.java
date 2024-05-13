f81e28816ca008d10ed38e2d247b19b1edce502f
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2024-05-13 01:29:57 -0300

    Seguir con el proyecto mañana!

--- Conexion/src/SistemaDeAgendaElectronica/BD/Contactos.java
+++ Conexion/src/SistemaDeAgendaElectronica/BD/Contactos.java
@@ -5,11 +5,36 @@
 package SistemaDeAgendaElectronica.BD;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  *
  * @author LENOVO
  */
 public class Contactos {
-   
+    Boolean logueado = null;
+    PreparedStatement instruccion = null;
+    ResultSet resultado = null;
+    Boolean registrado=null;
+    Conexion conexion=new Conexion();
+    Connection sl=conexion.conectar();
+
+
+public Boolean agregarContactos(String nombre,String telefono){
+    
+    
+   try {
+    String consulta="INSERT INTO user (nombre,telefono) VALUES (?,?,?)";
+    instruccion=sl.prepareStatement(consulta);
+    
+    } catch (SQLException e) {
+       System.out.println(""+e);
+    } finally {
+    }
+        
+         //Seguir mañana terminando todo esto!
+   return true;
+ }
 }
